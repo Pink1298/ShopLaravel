09e185cee49725e874466bde16bc1e5290df08f2
pink1298 <heocon98tt@gmail.com>
2021-02-24 19:09:45 +0700

    add theme frontend

--- xemaymeta.vn/app/Http/Controllers/Frontend/FrontendController.php
+++ xemaymeta.vn/app/Http/Controllers/Frontend/FrontendController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\HangXe; 
+use DB;
 
 class FrontendController extends Controller
 {
@@ -12,9 +14,38 @@ class FrontendController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    /**
+     * Action hiển thị view Trang chủ
+     * GET /
+     */
+    public function index(Request $request)
     {
-        //
+        // Query top 3 loại sản phẩm (có sản phẩm) mới nhất
+        $ds_top3_newest_loaisanpham = DB::table('hangxe')
+            ->join('xe', 'hangxe.hangxe_ma', '=', 'xe.hangxe_ma')
+            ->orderBy('hangxe_capNhat')->take(3)->get();
+
+        // Query tìm danh sách sản phẩm
+        $danhsachsanpham = $this->searchSanPham($request);
+        // Hiển thị view `frontend.index` với dữ liệu truyền vào
+        return view('frontend.index')
+            ->with('ds_top3_newest_loaisanpham', $ds_top3_newest_loaisanpham)
+            ->with('danhsachsanpham', $danhsachsanpham);
+    }
+
+    /**
+     * Hàm query danh sách sản phẩm theo nhiều điều kiện
+     */
+    private function searchSanPham(Request $request)
+    {
+        $query = DB::table('xe')->select('*');
+        // Kiểm tra điều kiện `searchByLoaiMa`
+        $searchByLoaiMa = $request->query('searchByLoaiMa');
+        if ($searchByLoaiMa != null) {
+        }
+
+        $data = $query->get();
+        return $data;
     }
 
     /**
